b54a8f82f14e7d2fcf69cfbfd30fed8139badac9
Wess58 <wesleymbate@gmail.com>
2018-11-01 12:57:45 +0300

    used appropriate methods for each file

--- app/src/main/java/com/wess58/myrestaurant/MainActivity.java
+++ app/src/main/java/com/wess58/myrestaurant/MainActivity.java
@@ -1,13 +1,71 @@
 package com.wess58.myrestaurant;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.textView) TextView mAppNameTextView;
+    @BindView(R.id.editText) EditText mLocationEditText;
+    @BindView(R.id.button2) Button mToastActionButton;
+    @BindView(R.id.button) Button mFindRestaurantsButton;
+    @BindView(R.id.button3) Button mMadLibsButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        //using fonts
+
+        Typeface keniaFonts = Typeface.createFromAsset(getAssets(),"fonts/Kenia/Kenia-Regular.ttf");
+        mAppNameTextView.setTypeface(keniaFonts);
+
+
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+              public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
+
+                //intent to maneuver to another page/activity class
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+
+
+        });
+
+
+          mToastActionButton.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                  Toast.makeText(MainActivity.this, "Welcome!!", Toast.LENGTH_SHORT).show();
+              }
+          });
+
+
+          mMadLibsButton.setOnClickListener(new View.OnClickListener(){
+              @Override
+                public void onClick(View v){
+                 Intent intent  = new Intent(MainActivity.this, MadLibsActivity.class);
+                 startActivity(intent);
+              }
+        });
+
+
     }
 }
---
+++ app/src/test/java/com/wess58/myrestaurant/MainActivityTest.java
@@ -0,0 +1,7 @@
+package com.wess58.myrestaurant;
+
+import org.junit.runner.RunWith;
+
+
+public class MainActivityTest {
+}
